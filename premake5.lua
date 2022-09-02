fdf0cb735c03b516fc95ada41473a70009d21fc4
benfc1993 <41189326+benfc1993@users.noreply.github.com>
2022-09-02 23:04:43 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+    "internal.h",
+    "imgui_tables.cpp",
+    "imgui_widgets.cpp",
+    "imgui_rectpack.h",
+    "imgui_textedit.h",
+    "imgui_truetype.h",
+    "imgui_demo.cpp"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+    cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:windows"
+		systemversion "latest"
+    cppdialect "C++17"
+		staticruntime "On"
