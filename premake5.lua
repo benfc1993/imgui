b81fb66496ad756062237cbe4155d0b4788d1b0c
Ben <benfeldbergcollins@hotmail.co.uk>
2022-09-04 22:25:08 +0100

    changes to premake

--- premake5.lua
+++ premake5.lua
@@ -8,28 +8,33 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
+		"imgui.h",
 		"imgui.cpp",
 		"imgui_draw.cpp",
-    	"internal.h",
+		"imgui_internal.h",
 		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
-		"imgui_rectpack.h",
-		"imgui_textedit.h",
-		"imgui_truetype.h",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
 		"imgui_demo.cpp"
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-    	cppdialect "C++17"
-		staticruntime "On"
-
 	filter "system:windows"
 		systemversion "latest"
-    	cppdialect "C++17"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
 		staticruntime "On"
 
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
